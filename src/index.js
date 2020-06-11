e0135d9a1e1d7723c7d4890af7b5058ac18e2380
Diego Romero <dr87152@gmail.com>
2020-06-11 12:05:59 -0500

    nada

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import router from '../src/routes/index';
-import CSS from "../src/styles/main.css";
+
 //cuando el archivo se cargue se va a mandar a llamar este archivo
 
 window.addEventListener('load',router);
